980d818d055c8487e93b2d93f23e405f2d13a73f
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-10-18 22:14:23 +0200

    Configure React app for deployment to GitHub Pages

---
+++ src/Tilte.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tilte = () => {
+  return <h1>To Do List</h1>;
+};
+
+export default Tilte;
---
+++ src/ToDoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ToDo from "./ToDo";
+
+const ToDoList = ({ toDoList, toggle }) => {
+  return (
+    <div>
+      {toDoList.map((todo) => {
+        return <ToDo todo={todo} toggle={toggle} />;
+      })}
+    </div>
+  );
+};
+
+export default ToDoList;
